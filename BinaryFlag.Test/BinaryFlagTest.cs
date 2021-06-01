f21dbfb9efb9d48eb98dffab1f2665cf43c6b7bf
Pienskoi <vladymyrpenskoy@gmail.com>
2021-06-01 19:09:24 +0300

    Lab 3 Initial Commit

---
+++ BinaryFlag.Test/BinaryFlagTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinaryFlag.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
