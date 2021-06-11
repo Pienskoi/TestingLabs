5c5fe3c10b4ef82338e929f5d9e0f0e633654c0c
Pienskoi <vladymyrpenskoy@gmail.com>
2021-06-12 00:50:36 +0300

    Add Lab 4 Tests

--- Integration.Test/FlagpoleTest.cs
+++ Integration.Test/FlagpoleTest.cs
@@ -1,3 +1,5 @@
+using IIG.CoSFE.DatabaseUtils;
+using IIG.BinaryFlag;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Integration.Test
@@ -5,9 +7,117 @@ namespace Integration.Test
     [TestClass]
     public class FlagpoleTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private const string Server = @"DESKTOP-RPD8Q03\SQLEXPRESS";
+        private const string Database = @"IIG.CoSWE.FlagpoleDB";
+        private const bool IsTrusted = false;
+        private const string Login = "sa";
+        private const string Password = @"L}EjpfCgru9X@GLj";
+        private const int ConnectionTimeout = 75;
+
+        private static FlagpoleDatabaseUtils DbUtils;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
         {
+            DbUtils = new FlagpoleDatabaseUtils(
+                Server, Database, IsTrusted, Login, Password, ConnectionTimeout);
+        }
+
+        [TestMethod]
+        public void Test_AddFlag_GetFlag_TrueInit_True()
+        {
+            var flag = new MultipleBinaryFlag(50);
+            var flagView = flag.ToString();
+            var flagValue = (bool)flag.GetFlag();
+
+            Assert.IsTrue(DbUtils.AddFlag(flagView, flagValue), 
+                "AddFlag True Binary Flag Wrong Result!");
+
+            var flagId = (int)DbUtils.GetIntBySql(
+                "SELECT TOP(1) MultipleBinaryFlagID " +
+                "FROM [dbo].[MultipleBinaryFlags] " +
+                "WHERE MultipleBinaryFlagView = " +
+                $"'{flagView}'");
+
+            Assert.IsTrue(DbUtils.GetFlag(flagId, out string outFlagView, out bool? outFlagValue),
+                "GetFlag True Binary Flag Wrong Result!");
+            Assert.AreEqual(flagView, outFlagView,
+                "True Flag Input and Output FlagViews Are Not Equal!");
+            Assert.AreEqual(flagValue, outFlagValue,
+                "True Flag Input and Output FlagValues Are Not Equal!");
+        }
+
+        [TestMethod]
+        public void Test_AddFlag_GetFlag_FalseInit_True()
+        {
+            var flag = new MultipleBinaryFlag(50, false);
+            var flagView = flag.ToString();
+            var flagValue = (bool)flag.GetFlag();
+
+            Assert.IsTrue(DbUtils.AddFlag(flagView, flagValue),
+                "AddFlag False Binary Flag Wrong Result!");
+
+            var flagId = (int)DbUtils.GetIntBySql(
+                "SELECT TOP(1) MultipleBinaryFlagID " +
+                "FROM [dbo].[MultipleBinaryFlags] " +
+                "WHERE MultipleBinaryFlagView = " +
+                $"'{flagView}'");
+
+            Assert.IsTrue(DbUtils.GetFlag(flagId, out string outFlagView, out bool? outFlagValue),
+                "GetFlag False Binary Flag Wrong Result!");
+            Assert.AreEqual(flagView, outFlagView,
+                "False Flag Input and Output FlagViews Are Not Equal!");
+            Assert.AreEqual(flagValue, outFlagValue,
+                "False Flag Input and Output FlagValues Are Not Equal!");
+        }
+
+        [TestMethod]
+        public void Test_AddFlag_GetFlag_TrueInit_Changed_True()
+        {
+            var flag = new MultipleBinaryFlag(50);
+            flag.ResetFlag(10);
+            var flagView = flag.ToString();
+            var flagValue = (bool)flag.GetFlag();
+
+            Assert.IsTrue(DbUtils.AddFlag(flagView, flagValue),
+                "AddFlag Changed Binary Flag Wrong Result!");
+
+            var flagId = (int)DbUtils.GetIntBySql(
+                "SELECT TOP(1) MultipleBinaryFlagID " +
+                "FROM [dbo].[MultipleBinaryFlags] " +
+                "WHERE MultipleBinaryFlagView = " +
+                $"'{flagView}'");
+
+            Assert.IsTrue(DbUtils.GetFlag(flagId, out string outFlagView, out bool? outFlagValue),
+                "GetFlag Changed Binary Flag Wrong Result!");
+            Assert.AreEqual(flagView, outFlagView,
+                "Changed Flag Input and Output FlagViews Are Not Equal!");
+            Assert.AreEqual(flagValue, outFlagValue,
+                "Changed Flag Input and Output FlagValues Are Not Equal!");
+        }
+
+        [TestMethod]
+        public void Test_AddFlag_LongFlagView_True()
+        {
+            var flag = new MultipleBinaryFlag(300_000_000);
+            var flagView = flag.ToString();
+            var flagValue = (bool)flag.GetFlag();
+
+            Assert.IsTrue(DbUtils.AddFlag(flagView, flagValue),
+                "AddFlag True Binary Flag with Big Length Wrong Result!");
+
+            var flagId = (int)DbUtils.GetIntBySql(
+                "SELECT TOP(1) MultipleBinaryFlagID " +
+                "FROM [dbo].[MultipleBinaryFlags] " +
+                "WHERE MultipleBinaryFlagView = " +
+                $"'{flagView}'");
+
+            Assert.IsTrue(DbUtils.GetFlag(flagId, out string outFlagView, out bool? outFlagValue),
+                "GetFlag True Binary Flag with Big Length Wrong Result!");
+            Assert.AreEqual(flagView, outFlagView,
+                "True Flag with Big Length Input and Output FlagViews Are Not Equal!");
+            Assert.AreEqual(flagValue, outFlagValue,
+                "True Flag with Big Length Input and Output FlagValues Are Not Equal!");
         }
     }
 }
---
+++ Integration.Test/StorageTest.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using IIG.CoSFE.DatabaseUtils;
+using IIG.FileWorker;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Integration.Test
+{
+    [TestClass]
+    public class StorageTest
+    {
+        private const string Server = @"DESKTOP-RPD8Q03\SQLEXPRESS";
+        private const string Database = @"IIG.CoSWE.StorageDB";
+        private const bool IsTrusted = false;
+        private const string Login = "sa";
+        private const string Password = @"L}EjpfCgru9X@GLj";
+        private const int ConnectionTimeout = 75;
+
+        private const string RootDirectory = @"\IntegrationTest_TempDir";
+        private static StorageDatabaseUtils DbUtils;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            DbUtils = new StorageDatabaseUtils(
+                Server, Database, IsTrusted, Login, Password, ConnectionTimeout);
+            BaseFileWorker.MkDir(RootDirectory);
+        }
+
+        [TestMethod]
+        public void Test_AddFile_GetFile_True()
+        {
+            var path = RootDirectory + @"\TestAddFile";
+            BaseFileWorker.Write("TestAddFile_Content", path);
+            var fileName = BaseFileWorker.GetFileName(path);
+            var fileContent = Encoding.Unicode.GetBytes(BaseFileWorker.ReadAll(path));
+
+            Assert.IsTrue(DbUtils.AddFile(fileName, fileContent), 
+                "AddFile Wrong Result!");
+
+            var flagId = (int)DbUtils.GetIntBySql(
+                "SELECT TOP(1) FileID " +
+                "FROM [dbo].[Files] " +
+                "WHERE FileName = " +
+                $"'{fileName}'");
+
+            Assert.IsTrue(DbUtils.GetFile(flagId, out string outFileName, out byte[] outFileContent), 
+                "GetFile Wrong Result!");
+            Assert.AreEqual(fileName, outFileName,
+                "Input and Output FileNames Are Not Equal!");
+            CollectionAssert.AreEqual(fileContent, outFileContent,
+                "Input and Output FileContents Are Not Equal!");
+        }
+
+        [TestMethod]
+        public void Test_AddFile_GetFile_LongFileName_True()
+        {
+            var sb = new StringBuilder();
+            var path = RootDirectory + @"\" + sb.Append('x', 255).ToString();
+            BaseFileWorker.Write("TestLongFileName_Content", path);
+            var fileName = BaseFileWorker.GetFileName(path);
+            var fileContent = Encoding.Unicode.GetBytes(BaseFileWorker.ReadAll(path));
+            var fileContentString = "0x" + $"{BitConverter.ToString(fileContent).Replace("-", "")}";
+
+            Assert.IsTrue(DbUtils.AddFile(fileName, fileContent), 
+                "AddFile with Long FileName Wrong Result!");
+
+            var flagId = (int)DbUtils.GetIntBySql(
+                "SELECT TOP(1) FileID " +
+                "FROM [dbo].[Files] " +
+                "WHERE FileContent = " +
+                $"{fileContentString}");
+
+            Assert.IsTrue(DbUtils.GetFile(flagId, out string outFileName, out byte[] outFileContent),
+                "GetFile with Long FileName Wrong Result!");
+            Assert.AreEqual(fileName, outFileName,
+                "Input and Output Long FileNames Are Not Equal!");
+            CollectionAssert.AreEqual(fileContent, outFileContent,
+                "Input and Output FileContents of File with Long FileName Are Not Equal!");
+        }
+
+        [TestMethod]
+        public void Test_AddFile_GetFile_LongFileContent_True()
+        {
+            var sb = new StringBuilder();
+            var path = RootDirectory + @"\TestLongFileContent";
+            BaseFileWorker.Write(sb.Append('x', 513).ToString(), path);
+            var fileName = BaseFileWorker.GetFileName(path);
+            var fileContent = Encoding.Unicode.GetBytes(BaseFileWorker.ReadAll(path));
+
+            Assert.IsTrue(DbUtils.AddFile(fileName, fileContent), 
+                "AddFile with Long FileContent Wrong Result!");
+
+            var flagId = (int)DbUtils.GetIntBySql(
+                "SELECT TOP(1) FileID " +
+                "FROM [dbo].[Files] " +
+                "WHERE FileName = " +
+                $"'{fileName}'");
+
+            Assert.IsTrue(DbUtils.GetFile(flagId, out string outFileName, out byte[] outFileContent),
+                "GetFile with Long FileContent Wrong Result!");
+            Assert.AreEqual(fileName, outFileName,
+                "Input and Output FileNames of File with Long FileContent Are Not Equal!");
+            CollectionAssert.AreEqual(fileContent, outFileContent,
+                "Input and Output Long FileContents Are Not Equal!");
+        }
+
+        [TestMethod]
+        public void Test_AddFile_DeleteFile_True()
+        {
+            var path = RootDirectory + @"\TestDeleteFile";
+            BaseFileWorker.Write("TestDeleteFil_Content", path);
+            var fileName = BaseFileWorker.GetFileName(path);
+            var fileContent = Encoding.Unicode.GetBytes(BaseFileWorker.ReadAll(path));
+            
+            DbUtils.AddFile(fileName, fileContent);
+            var fileId = (int)DbUtils.GetIntBySql(
+                "SELECT TOP(1) FileID " +
+                "FROM [dbo].[Files] " +
+                "WHERE FileName = " +
+                $"'{fileName}'");
+
+            Assert.IsTrue(DbUtils.DeleteFile(fileId), 
+                "DeleteFile Wrong Result!");
+            Assert.IsFalse(DbUtils.GetFile(fileId, out _, out _), 
+                "File Was Not Deleted from Database!");
+        }
+
+        [TestMethod]
+        public void Test_AddFile_GetFiles_OneFile_Valid()
+        {
+            var path = RootDirectory + @"\TestGetFiles_OneFile";
+            BaseFileWorker.Write("TestGetFiles_Content", path);
+            var fileName = BaseFileWorker.GetFileName(path);
+            var fileContent = Encoding.Unicode.GetBytes(BaseFileWorker.ReadAll(path));
+
+            DbUtils.AddFile(fileName, fileContent);
+
+            var dt = DbUtils.GetFiles(fileName);
+
+            Assert.AreEqual(1, dt.Rows.Count, 
+                "GetFiles on One Unique File Returns Wrong Amount of Files!");
+            var itemArray = dt.Rows[0].ItemArray;
+            Assert.AreEqual(fileName, itemArray[1] as string,
+                "Input and Output FileNames Are Not Equal!");
+            CollectionAssert.AreEqual(fileContent, itemArray[2] as byte[],
+                "Input and Output FileContents Are Not Equal!");
+        }
+
+        [TestMethod]
+        public void Test_AddFile_GetFiles_MultipleFiles_Valid()
+        {
+            var path1 = RootDirectory + @"\TestGetFiles_MultipleFiles";
+            BaseFileWorker.Write("First_TestGetFiles_Content", path1);
+            var fileName1 = BaseFileWorker.GetFileName(path1);
+            var fileContent1 = Encoding.Unicode.GetBytes(BaseFileWorker.ReadAll(path1));
+            BaseFileWorker.MkDir(RootDirectory + @"\SubDirectory");
+            var path2 = RootDirectory + @"\SubDirectory\TestGetFiles_MultipleFiles";
+            BaseFileWorker.Write("Second_TestGetFiles_Content", path2);
+            var fileName2 = BaseFileWorker.GetFileName(path2);
+            var fileContent2 = Encoding.Unicode.GetBytes(BaseFileWorker.ReadAll(path2));
+
+            DbUtils.AddFile(fileName1, fileContent1);
+            Assert.IsTrue(DbUtils.AddFile(fileName2, fileContent2), 
+                "AddFile with Same FileNames Wrong Result!");
+
+            var dt = DbUtils.GetFiles(fileName1);
+
+            Assert.AreEqual(2, dt.Rows.Count,
+                "GetFiles on Two Files with Same FileName Returns Wrong Amount of Files!");
+            var itemArray1 = dt.Rows[0].ItemArray;
+            Assert.AreEqual(fileName1, itemArray1[1] as string,
+                "Input and Output FileNames of First File with Same FileName Are Not Equal!");
+            CollectionAssert.AreEqual(fileContent1, itemArray1[2] as byte[],
+                "Input and Output FileContents of First File with Same FileName Are Not Equal!");
+            var itemArray2 = dt.Rows[1].ItemArray;
+            Assert.AreEqual(fileName2, itemArray2[1] as string,
+                "Input and Output FileNames of Second File with Same FileName Are Not Equal!");
+            CollectionAssert.AreEqual(fileContent2, itemArray2[2] as byte[],
+                "Input and Output FileContents of Second File with Same FileName Are Not Equal!");
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Directory.Delete(RootDirectory, true);
+        }
+    }
+}
